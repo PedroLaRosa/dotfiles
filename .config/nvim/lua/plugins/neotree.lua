c08eb9d8bd676494f7b7db15ca913604eec691db
Pedro La Rosa D <pedrolarosad@gmail.com>
2024-09-29 02:10:58 +0200

    chore(nvim-tree): remove custom config for nvim-tree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -4,23 +4,21 @@ return {
   init = function()
     vim.g.neotree = {
       auto_close = true,
-      auto_open = false,
+      auto_open = true,
       auto_update = true,
       update_to_buf_dir = true,
     }
   end,
 
-  -- merged with the default configuration
   opts = {
     close_if_last_window = true,
-
+    window = { position = "right" },
     filesystem = {
       filtered_items = {
         hide_dotfiles = false,
         hide_gitignored = false,
       },
     },
-
     hijack_netrw_behavior = "disabled",
   },
 }
