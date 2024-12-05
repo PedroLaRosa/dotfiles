b8106bae0c8663ace1be56d8c11c8525ce378a41
Pedro La Rosa D <pedrolarosad@gmail.com>
2024-12-05 17:02:31 +0100

    feat(nvim): customize the snack dashboard

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,1 +1,36 @@
-return { "folke/snacks.nvim", opts = { dashboard = { enabled = false } } }
+return {
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = {
+      enabled = true,
+      sections = {
+        { section = "header" },
+        {
+          pane = 2,
+          section = "terminal",
+          cmd = "curl https://wttr.in/",
+          height = 8,
+          padding = 1,
+        },
+        { section = "keys", gap = 1, padding = 1 },
+        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+        -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+        {
+          pane = 2,
+          icon = " ",
+          title = "Git Status",
+          section = "terminal",
+          enabled = function()
+            return Snacks.git.get_root() ~= nil
+          end,
+          cmd = "hub status --short --branch --renames",
+          height = 5,
+          padding = 1,
+          ttl = 5 * 60,
+          indent = 3,
+        },
+        { section = "startup" },
+      },
+    },
+  },
+}
