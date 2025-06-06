cf805750441c8cd027da0de75650ed66ec51843a
Pedro La Rosa D <pedrolarosad@gmail.com>
2025-06-07 01:24:09 +0200

    chore(brew): add pnpm

--- brew.sh
+++ brew.sh
@@ -51,6 +51,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && \. "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"                                       # This loads nvm
 [ -s "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" ] && \. "$HOMEBREW_PREFIX/opt/nvm/etc/bash_completion.d/nvm" # This loads nvm bash_completion
 
+# Install PNPM
+brew install pnpm
+
 # Install neovim and LazyVim Dependencies
 brew install neovim
 brew install rust-analyzer
