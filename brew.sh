59612f0caf2d4ca330995c1992c7f338695f9c72
Pedro La Rosa D <pedrolarosad@gmail.com>
2025-09-17 17:34:08 +0200

    chore(brew): add imagemagick as cli tool

--- brew.sh
+++ brew.sh
@@ -97,6 +97,9 @@ brew install git
 # Install github cli
 brew install gh
 
+# Install imagemagick; Tools and libraries to manipulate images in many formats
+brew install imagemagick
+
 # Glances
 brew install glances
 
