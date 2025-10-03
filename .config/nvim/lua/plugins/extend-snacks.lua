d90b1bfdb97594dd990fdb12768c82594d4afaa4
Pedro La Rosa D <pedrolarosad@gmail.com>
2025-10-03 10:04:35 +0200

    feat(nvim): new dashboard panel

--- .config/nvim/lua/plugins/extend-snacks.lua
+++ .config/nvim/lua/plugins/extend-snacks.lua
@@ -22,24 +22,88 @@ return {
           padding = 1,
         },
         { section = "keys", gap = 1, padding = 1 },
-        { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-        -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
         {
           pane = 2,
-          icon = " ",
-          title = "Git Status",
-          section = "terminal",
-          enabled = function()
-            return Snacks.git.get_root() ~= nil
-          end,
-          cmd = "hub status --short --branch --renames",
-          height = 5,
+          icon = " ",
+          desc = "Browse Repo",
           padding = 1,
-          ttl = 5 * 60,
-          indent = 3,
+          key = "b",
+          action = function()
+            Snacks.gitbrowse()
+          end,
         },
+        function()
+          local in_git = Snacks.git.get_root() ~= nil
+          local cmds = {
+            {
+              title = "Notifications",
+              cmd = "gh notify -s -an 5",
+              action = function()
+                vim.ui.open("https://github.com/notifications")
+              end,
+              key = "n",
+              icon = " ",
+              height = 10,
+              enabled = true,
+            },
+            {
+              icon = " ",
+              title = "Open PRs",
+              cmd = "gh pr list -L 5",
+              key = "P",
+              action = function()
+                vim.fn.jobstart("gh pr list --web", { detach = true })
+              end,
+              height = 7,
+            },
+            {
+              icon = " ",
+              title = "Git Status",
+              cmd = "git --no-pager diff --stat -B -M -C",
+              height = 10,
+            },
+          }
+          return vim.tbl_map(function(cmd)
+            return vim.tbl_extend("force", {
+              pane = 2,
+              section = "terminal",
+              enabled = in_git,
+              padding = 1,
+              ttl = 5 * 60,
+              indent = 3,
+            }, cmd)
+          end, cmds)
+        end,
         { section = "startup" },
       },
+      -- sections = {
+      --   { section = "header" },
+      --   {
+      --     pane = 2,
+      --     section = "terminal",
+      --     cmd = "curl https://wttr.in/",
+      --     height = 8,
+      --     padding = 1,
+      --   },
+      --   { section = "keys", gap = 1, padding = 1 },
+      --   { pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
+      --   -- { pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
+      --   {
+      --     pane = 2,
+      --     icon = " ",
+      --     title = "Git Status",
+      --     section = "terminal",
+      --     enabled = function()
+      --       return Snacks.git.get_root() ~= nil
+      --     end,
+      --     cmd = "hub status --short --branch --renames",
+      --     height = 5,
+      --     padding = 1,
+      --     ttl = 5 * 60,
+      --     indent = 3,
+      --   },
+      --   { section = "startup" },
+      -- },
     },
   },
 }
--- .zshrc
+++ .zshrc
@@ -142,3 +142,4 @@ source /opt/homebrew/share/powerlevel10k/powerlevel10k.zsh-theme
 
 # Amazon Q post block. Keep at the bottom of this file.
 [[ -f "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh" ]] && builtin source "${HOME}/Library/Application Support/amazon-q/shell/zshrc.post.zsh"
+export PATH="$HOME/.local/bin:$PATH"
